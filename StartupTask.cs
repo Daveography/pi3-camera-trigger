42e7bdfe1f47ede8c34a68ac919711678e49f30f
Dave Sutherland <dave@daveography.ca>
2020-04-17 11:23:17 -0600

    Swap pins to make cleaner HAT and add new diagrams

--- StartupTask.cs
+++ StartupTask.cs
@@ -11,9 +11,9 @@ namespace Pi3CameraTrigger
 {
     public sealed class StartupTask : IBackgroundTask
     {
-        private const int MOTION_SENSOR_PIN = 26;
+        private const int MOTION_SENSOR_PIN = 23;
         private const int CAMERA_FOCUS_PIN = 18;
-        private const int CAMERA_SHUTTER_PIN = 23;
+        private const int CAMERA_SHUTTER_PIN = 27;
 
         private const double CAMERA_FOCUS_SECONDS = 0.5;
         private const double CAMERA_SHUTTER_SECONDS = 0.1;
